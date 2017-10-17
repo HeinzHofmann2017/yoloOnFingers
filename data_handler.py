17a0b9ea59c3543b36ed444c1ce59f8dd9509fdb
Heinz Hofmann <hhofmann@hsr.ch>
2017-10-17 15:30:07 +0200

    Neuronal Network designed after yolo, but with little adaption, for pictures in our size

---
+++ data_handler.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 15:41:13 2017
+
+@author: hhofmann
+"""
+class datahandler(object)
+    def __init__(self,path):
+        self.big_dataset()
+        
+        return Data_object
+    
+    
+    def next_batch(batch_size):
+            
+        return batch
