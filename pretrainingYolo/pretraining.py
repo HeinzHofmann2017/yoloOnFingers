ce91d92a39336e8c546a19b74eb9548a31ee5e13
Heinz Hofmann <hhofmann@hsr.ch>
2017-11-03 16:37:48 +0100

    corrected something wrong with the mailer

--- pretrainingYolo/pretraining.py
+++ pretrainingYolo/pretraining.py
@@ -444,9 +444,9 @@ def main():
                             nr_of_matches += 1
                 match_probability = 100 * nr_of_matches / batchSize
                 print("How well does the training-Prediction match on the Labels: " + str(match_probability)+" %")
-                if(nr_of_matches > training_matches):
+                if(match_probability > training_matches):
                     training_matches+=3
-                    mailer.mailto("Number of Matches in the training Set reached (with higher learnrat(0.05)) "+str(nr_of_matches)+" %. Done in "+ str(i)+ " Steps")
+                    mailer.mailto("Number of Matches in the training Set reached (with higher learnrat(0.05)) "+str(match_probability)+" %. Done in "+ str(i)+ " Steps")
                 
                 #validation:
                 sess.run(validation_init_op)
@@ -460,9 +460,9 @@ def main():
                             nr_of_matches += 1
                 match_probability = 100 * nr_of_matches / batchSize
                 print("How well does the validation-Prediction match on the Labels: " + str(match_probability)+" %")
-                if(nr_of_matches > validation_matches):
+                if(match_probability > validation_matches):
                     validation_matches+=3
-                    mailer.mailto("Number of Matches in the validation Set reached (with higher learnrat(0.05)) "+str(nr_of_matches)+" %. Done in "+ str(i)+ " Steps")
+                    mailer.mailto("Number of Matches in the validation Set reached (with higher learnrat(0.05)) "+str(match_probability)+" %. Done in "+ str(i)+ " Steps")
                 sess.run(training_init_op)
 
                 saver.save(sess=sess, save_path=origin_path + "../../getfingers_heinz/weights/pretrain_model.ckpt", global_step=i)
