ceb5add718001c8d8178c6e7ae26be70fd599a1a
hhofmann <hhofmann@wx-el-119730.hsr.ch>
2017-11-03 16:16:41 +0100

    added sh file for open docker container

---
+++ runDocker/7pretrain.sh
@@ -0,0 +1,9 @@
+NV_GPU=7  \
+nvidia-docker run --rm -t -i \
+-v /home/hhofmann/data:/mnt/data \
+-v /mnt/iscsi:/mnt/fast \
+-v /home/hhofmann/code:/workspace/code \
+-u $(id -u):$(id -g) \
+nvcr.io/nvidia/tensorflow:17.09 \
+python code/pretrainingYolo/pretraining.py
+#for make directly something inside the docker-container, make a backslash after 17.09 and type the command you want to have processed on a new line. 
