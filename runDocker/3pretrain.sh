7606420df09fd44ae93f6b1b5a096987f99a2679
Heinz Hofmann <hhofmann@hsr.ch>
2017-11-09 16:19:26 +0100

    update models on GPU3 more often (all 100 epochs instead of all 1000)

--- runDocker/3pretrain.sh
+++ runDocker/3pretrain.sh
@@ -12,5 +12,5 @@ python code/pretrainingYolo/pretraining.py  --name $1 \
                                             --bufferSize 100000 \
                                             --originPath "/mnt/data/ilsvrc2012/LabelList_Heinz/" \
                                             --nrOfEpochs 10000000 \
-                                            --nrOfEpochsUntilSaveModel 1000
+                                            --nrOfEpochsUntilSaveModel 100
 #for make directly something inside the docker-container, make a backslash after 17.09 and type the command you want to have processed on a new line. 
