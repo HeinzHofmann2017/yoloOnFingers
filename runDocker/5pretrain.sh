62acc212bed41231659a692a61a6627da632a65b
Heinz Hofmann <hhofmann@hsr.ch>
2017-11-10 15:47:19 +0100

    used the wrong GPU

--- runDocker/5pretrain.sh
+++ runDocker/5pretrain.sh
@@ -7,10 +7,10 @@ nvidia-docker run --rm -t -i \
 nvcr.io/nvidia/tensorflow:17.09 \
 python code/pretrainingYolo/pretraining.py  --name $1 \
                                             --learningrate 0.001 \
-                                            --batchSize 16 \
+                                            --batchSize 128 \
                                             --numThreads 16 \
                                             --bufferSize 100000 \
                                             --originPath "/mnt/data/ilsvrc2012/LabelList_Heinz/" \
-                                            --nrOfEpochs 100 \
-                                            --nrOfEpochsUntilSaveModel 10
+                                            --nrOfEpochs 10000000 \
+                                            --nrOfEpochsUntilSaveModel 1000
 #for make directly something inside the docker-container, make a backslash after 17.09 and type the command you want to have processed on a new line. 
