ad625125b71106e26ff167bd8849cde457381722
Heinz Hofmann <hhofmann@hsr.ch>
2017-11-22 14:48:12 +0100

    only dropout didn't work, now try only batchnorm...

--- fingerYolo/5train.sh
+++ fingerYolo/5train.sh
@@ -13,5 +13,5 @@ python code/fingerYolo/training.py          --name $1 \
                                             --originPath "/mnt/data/getfingers_heinz/Data/indexfinger_right/3000_readyTOlearn/trainData/" \
                                             --nrOfEpochs 10000000 \
                                             --nrOfEpochsUntilSaveModel 1000 \
-                                            --noBatchnorm
+                                            --noDropout
 #for make directly something inside the docker-container, make a backslash after 17.09 and type the command you want to have processed on a new line. 
