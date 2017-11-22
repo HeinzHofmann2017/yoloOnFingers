fc029fe9c365bba1d7d5e1758964b897cd311316
Heinz Hofmann <hhofmann@hsr.ch>
2017-11-22 14:57:55 +0100

    batchsize reduced to 1 and enabled dropout again ...

--- fingerYolo/5train.sh
+++ fingerYolo/5train.sh
@@ -7,11 +7,10 @@ nvidia-docker run --rm -t -i \
 nvcr.io/nvidia/tensorflow:17.09 \
 python code/fingerYolo/training.py          --name $1 \
                                             --learningrate 0.001 \
-                                            --batchSize 8 \
+                                            --batchSize 1 \
                                             --numThreads 16 \
                                             --bufferSize 500 \
                                             --originPath "/mnt/data/getfingers_heinz/Data/indexfinger_right/3000_readyTOlearn/trainData/" \
                                             --nrOfEpochs 10000000 \
                                             --nrOfEpochsUntilSaveModel 1000 \
-                                            --noDropout
 #for make directly something inside the docker-container, make a backslash after 17.09 and type the command you want to have processed on a new line. 
