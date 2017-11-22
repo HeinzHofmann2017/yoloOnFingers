2db2aa0a06c5b1d239127bde1bdc380df8aeebd7
Heinz Hofmann <hhofmann@hsr.ch>
2017-11-22 15:18:02 +0100

    either batchsize=10 didnt work, so batchsize now is 8

--- fingerYolo/5train.sh
+++ fingerYolo/5train.sh
@@ -7,7 +7,7 @@ nvidia-docker run --rm -t -i \
 nvcr.io/nvidia/tensorflow:17.09 \
 python code/fingerYolo/training.py          --name $1 \
                                             --learningrate 0.001 \
-                                            --batchSize 10 \
+                                            --batchSize 8 \
                                             --numThreads 16 \
                                             --bufferSize 500 \
                                             --originPath "/mnt/data/getfingers_heinz/Data/indexfinger_right/3000_readyTOlearn/trainData/" \
