there was a bug with the calculation of the true-negatives and the false-negatives

fingerYolo/training.py
@@ -390,10 +390,11 @@ def main():
             tf.summary.histogram(var.op.name +"capped_gradients",grad)
             
     with tf.name_scope("Test") as scope:
-        true_probs = tf.multiply(p_output,p_label)
-        false_probs = tf.multiply(p_output,p_label_invers)
+        
+        
         total_nr_of_Gridcells = tf.reduce_sum(tf.maximum(tf.add(p_label,2),1))
-        with tf.name_scope("true_positives"):        
+        with tf.name_scope("true_positives"):     
+            true_probs = tf.multiply(p_output,p_label)
             normed_probs_tp = tf.subtract(true_probs,0.5)
             deleted_probs_tp = tf.maximum(normed_probs_tp,np.float16(0))
             residual_probs_tp = tf.multiply(deleted_probs_tp,1000.0)
@@ -401,7 +402,8 @@ def main():
             true_positives = tf.reduce_sum(true_positives)
             true_positives_normed = tf.div(true_positives,total_nr_of_Gridcells)
             tf.summary.scalar("true_positives",true_positives_normed)
-        with tf.name_scope("false_positives"):           
+        with tf.name_scope("false_positives"):   
+            false_probs = tf.multiply(p_output,p_label_invers)
             normed_probs_fp = tf.subtract(false_probs,0.5)
             deleted_probs_fp = tf.maximum(normed_probs_fp,np.float16(0))
             residual_probs_fp = tf.multiply(deleted_probs_fp,1000.0)
@@ -410,6 +412,8 @@ def main():
             false_positives_normed = tf.div(false_positives,total_nr_of_Gridcells)
             tf.summary.scalar("false_positives",false_positives_normed)
         with tf.name_scope("true_negatives"):
+            p_label_invers_1000 = tf.add(tf.multiply(p_label,999),1)
+            false_probs = tf.multiply(p_label_invers_1000,p_output)            
             normed_probs_tn = tf.subtract(false_probs,0.5)
             deleted_probs_tn = tf.minimum(normed_probs_tn,np.float16(0))
             residual_probs_tn = tf.multiply(deleted_probs_tn,-1000.0)
@@ -418,13 +422,15 @@ def main():
             true_negatives_normed = tf.div(true_negatives,total_nr_of_Gridcells)
             tf.summary.scalar("true_negatives",true_negatives_normed)
         with tf.name_scope("false_negatives"):
+            p_label_1000 = tf.add(tf.multiply(p_label_invers,999),1)
+            true_probs = tf.multiply(p_label_1000,p_output)            
             normed_probs_fn = tf.subtract(true_probs,0.5)
             deleted_probs_fn = tf.minimum(normed_probs_fn,np.float16(0))
             residual_probs_fn = tf.multiply(deleted_probs_fn,-1000.0)
             false_negatives = tf.minimum(residual_probs_fn,np.float16(1))
             false_negatives = tf.reduce_sum(false_negatives)
             false_negatives_normed = tf.div(false_negatives,total_nr_of_Gridcells)
-            tf.summary.scalar("flase_negatives",false_negatives_normed)
+            tf.summary.scalar("false_negatives",false_negatives_normed)
         
             
             