6b0293084a3eb21ba9c764db5d1bb6fcff22bf00
Heinz Hofmann <hhofmann@hsr.ch>
2017-12-13 14:30:59 +0100

    fixed the same dimension problem again at a different place ==> lets test again

--- fingerYolo/training.py
+++ fingerYolo/training.py
@@ -458,8 +458,8 @@ def main():
             x_label_index  = tf.argmax(tf.reduce_max(p_label,axis=[1]),axis=1)
             y_label_global = tf.add((tf.cast(y_label_index,tf.float32)/7) , (tf.reduce_max((y_label/7),axis=[1,2])))
             x_label_global = tf.add((tf.cast(x_label_index,tf.float32)/7) , (tf.reduce_max((x_label/7),axis=[1,2])))
-            h_label_global = h_label
-            w_label_global = w_label
+            h_label_global = tf.reduce_max(h_label,axis=[1,2])
+            w_label_global = tf.reduce_max(w_label,axis=[1,2])
             
             y_prob_index = tf.argmax(tf.reduce_max(p_output, axis=[2]),axis=1)
             x_prob_index = tf.argmax(tf.reduce_max(p_output, axis=[1]),axis=1)
