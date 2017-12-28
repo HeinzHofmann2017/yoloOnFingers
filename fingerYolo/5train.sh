5515b0a187ba7f1fa0f523cfb9115d9c21c5a4db
Heinz Hofmann <hhofmann@hsr.ch>
2017-12-28 19:30:19 +0100

    pretraining works fine with full-dropout & batchSize=100, but training not, so there I reduced the batchSize to 2(just for testing)

--- fingerYolo/5train.sh
+++ fingerYolo/5train.sh
@@ -7,7 +7,7 @@ nvidia-docker run --rm -t -i \
 nvcr.io/nvidia/tensorflow:17.09 \
 python code/fingerYolo/training.py          --name $1 \
                                             --learningrate 0.001 \
-                                            --batchSize 15 \
+                                            --batchSize 2 \
                                             --numThreads 16 \
                                             --bufferSize 100000 \
                                             --originPath "/mnt/data/data_hhofmann/Data/indexfinger_right/6000_readyTOlearn/trainData/" \
