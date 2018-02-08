ad73b062fba4632d2bc858021739b2fd851ce38a
Heinz Hofmann <hhofmann@hsr.ch>
2018-02-08 15:31:20 +0100

    deleted some last files.

--- helperfunctions/parsetest.sh
+++
@@ -1,8 +0,0 @@
-python2.7 parser.py 	--learningrate 10 \
-			--batchSize 10 \
-			--numThreads 10 \
-			--bufferSize 10 \
-			--originPath "Teststring" \
-			--name "Testname" \
-			--nrOfEpochs 10 \
-			--nrOfEpochsUntilSaveModel 10
--- helperfunctions/parsetestempty.sh
+++
@@ -1,8 +0,0 @@
-python2.7 parsetest.py 	--learningrate \
-			--batchSize \
-			--numThreads \
-			--bufferSize \
-			--originPath \
-			--name \
-			--nrOfEpochs \
-			--nrOfEpochsUntilSaveModel
