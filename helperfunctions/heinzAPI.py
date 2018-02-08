9857ca373b0ea5bebcf52d54d28c6f75c084e855
Heinz Hofmann <hhofmann@hsr.ch>
2018-02-08 15:18:50 +0100

    cleaned up the heinzAPI

--- helperfunctions/heinzAPI.py
+++ helperfunctions/heinzAPI.py
@@ -13,14 +13,9 @@ import pickle
 import numpy as np
 
 import tensorflow as tf
-from tensorflow.python.ops import array_ops
-from tensorflow.contrib.data import Dataset, Iterator
-from tensorflow.python.platform import gfile
 
 this_folder =  os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, this_folder+ '/../dataPreprocessing/OnILSVRCdata/')
-import analyse_Dataset
-import mailer
 
 def batchnorm(input_tensor):
     with tf.name_scope("batch_norm"):
@@ -52,15 +47,8 @@ def convLayer(tensor,layerNr,batchSize, filterwidth, inputdepth, outputdepth, st
     '''
     with tf.name_scope(str(layerNr)+"_conv_Layer") as scope:
         with tf.name_scope("W"):
-            #calculate stdev for weights, to pretend vanishing Gradients
-                        
-            #weightdev = (2 / (filterwidth*(inputdepth+outputdepth))) + 1e-4#get shure, that stdev don't will be zero
-            weightdev = 0.01
-            W = tf.Variable(tf.truncated_normal(shape=[filterwidth,filterwidth,inputdepth,outputdepth], stddev=weightdev, dtype=tf.float32))
-        #with tf.name_scope("b"):
-            #b = tf.Variable(tf.truncated_normal(shape=[outputdepth],stddev=0.01,dtype=tf.float32))
+            W = tf.Variable(tf.truncated_normal(shape=[filterwidth,filterwidth,inputdepth,outputdepth], stddev=0.01, dtype=tf.float32))
         preactivate = tf.nn.conv2d(input=tensor,filter=W,strides=[1,strides,strides,1],padding='SAME')
-        #preactivate = tf.add(preactivate, b)
         if batchnorm_ == True:
             preactivate = batchnorm(input_tensor=preactivate)
         if dropout_ == True:
@@ -70,11 +58,6 @@ def convLayer(tensor,layerNr,batchSize, filterwidth, inputdepth, outputdepth, st
                                   lambda:preactivate)
         with tf.name_scope("relu"):
             tensor = tf.maximum(0*preactivate,preactivate)
-#==============================================================================
-#         with tf.name_scope("leaky_relu"):
-#             tensor = tf.maximum(0.1*preactivate,preactivate)
-#==============================================================================
-
         with tf.name_scope("summary"):
             variable_summaries(variable=W,name="W"+str(layerNr))
             #variable_summaries(variable=b,name="b")
@@ -114,7 +97,6 @@ def normalize_pictures(tensor):
     
 def batchnormPretrained(input_tensor,layerNr,origin_path):
     with tf.name_scope("batch_norm"):
-        input_depth = input_tensor.get_shape().as_list()[-1]#takes the last element which is in this case 64
     #make new mean and new Variance
         with tf.name_scope("beta"):
             pythonbeta = pickle.load( open( origin_path + "../../../weights/pythonWeights/"+str(layerNr)+"_conv_Layer_batch_norm_beta_beta.pkl", "rb" ) )
@@ -144,30 +126,9 @@ def convLayerPretrained(tensor,layerNr,batchSize, filterwidth, inputdepth, outpu
     '''
     with tf.name_scope(str(layerNr)+"_conv_Layer") as scope:
         with tf.name_scope("W"):
-            #calculate stdev for weights, to pretend vanishing Gradients
-                        
-            #weightdev = (2 / (filterwidth*(inputdepth+outputdepth))) + 1e-4#get shure, that stdev don't will be zero
-            weightdev = 0.01
             pythonW = pickle.load( open( origin_path + "../../../weights/pythonWeights/"+str(layerNr)+"_conv_Layer_W_Variable.pkl", "rb" ) )
-#==============================================================================
-#             
-#             if layerNr <8:
-#                 print("\n\n\n\n start with Layer"+str(layerNr))
-#                 for q in np.nditer(pythonW):
-#                     if q>1:
-#                         print(q)
-#                 print("finished with layer "+str(layerNr))
-# 
-# #==============================================================================
-# #             #Test
-# #             pythonW[:,:,:]=1e15
-# #==============================================================================
-#==============================================================================
             W = tf.Variable(pythonW,dtype=tf.float32)
-        #with tf.name_scope("b"):
-            #b = tf.Variable(tf.truncated_normal(shape=[outputdepth],stddev=0.01,dtype=tf.float32))
-        preactivate = tf.nn.conv2d(input=tensor,filter=W,strides=[1,strides,strides,1],padding='SAME')
-        #preactivate = tf.add(preactivate, b)
+        preactivate = tf.nn.conv2d(input=tensor,filter=W,strides=[1,strides,strides,1],padding='SAME')        
         if batchnorm_ == True:
             preactivate = batchnormPretrained(input_tensor=preactivate,layerNr=layerNr,origin_path=origin_path)
         if dropout_ == True:
@@ -177,11 +138,6 @@ def convLayerPretrained(tensor,layerNr,batchSize, filterwidth, inputdepth, outpu
                                   lambda:preactivate)
         with tf.name_scope("relu"):
             tensor = tf.maximum(0*preactivate,preactivate)
-#==============================================================================
-#         with tf.name_scope("leaky_relu"):
-#             tensor = tf.maximum(0.1*preactivate,preactivate)
-#==============================================================================
-
         with tf.name_scope("summary"):
             variable_summaries(variable=W,name="W"+str(layerNr))
             #variable_summaries(variable=b,name="b")
